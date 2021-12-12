260ceba92c1097a5e36c9c32c362964c9d0deefe
TimVanMourik <timvanmourik@gmail.com>
2021-12-12 21:19:31 +0100

    day 10

--- src/day10/solution.ts
+++ src/day10/solution.ts
@@ -1,20 +1,84 @@
 import { parseStringList } from "../helpers/parseInput";
 import { Star } from "../star";
 
-const INPUT_FILE = "input/test.txt";
+const INPUT_FILE = "input/day10.txt";
+type OpeningBracket = "(" | "[" | "{" | "<";
+type ClosingBracket = ")" | "]" | "}" | ">";
+type Bracket = OpeningBracket | ClosingBracket;
+type Brackets = Bracket[];
 
-function parseInstructions(input: string): any {
+const bracketMap = new Map<Bracket, Bracket>();
+bracketMap.set("(", ")");
+bracketMap.set("[", "]");
+bracketMap.set("{", "}");
+bracketMap.set("<", ">");
+
+const illegalCharacterMap = new Map<Bracket, number>();
+illegalCharacterMap.set(")", 3);
+illegalCharacterMap.set("]", 57);
+illegalCharacterMap.set("}", 1197);
+illegalCharacterMap.set(">", 25137);
+
+const closingCharacterMap = new Map<Bracket, number>();
+closingCharacterMap.set("(", 1);
+closingCharacterMap.set("[", 2);
+closingCharacterMap.set("{", 3);
+closingCharacterMap.set("<", 4);
+
+function parseInstructions(input: string): Brackets[] {
   const lines = parseStringList(input);
-  return lines;
+  return lines.map((line) => line.split("") as Brackets);
 }
 
 export function day10(star: Star): number {
+  const brackets = parseInstructions(INPUT_FILE);
   switch (star) {
     case Star.First: {
-      return 0;
+      let error = 0;
+      for (const line of brackets) {
+        error += checkBrackets(line);
+      }
+
+      return error;
     }
     case Star.Second: {
-      return 0;
+      const scores: number[] = brackets
+        .filter((line) => checkBrackets(line) === 0)
+        .map((line) => findClosingTagScore(line));
+      scores.sort((a, b) => a - b);
+      return scores[(scores.length - 1) / 2];
     }
   }
+}
+
+function checkBrackets(brackets: Brackets): number {
+  const stack: Bracket[] = [];
+  for (const bracket of brackets) {
+    if (bracketMap.has(bracket)) {
+      stack.push(bracket);
+    } else {
+      const last = stack.pop()!;
+      if (bracket !== bracketMap.get(last)) {
+        return illegalCharacterMap.get(bracket)!;
+      }
+    }
+  }
+  return 0;
+}
+
+function findClosingTagScore(brackets: Brackets): number {
+  const stack: Bracket[] = [];
+  for (const bracket of brackets) {
+    if (bracketMap.has(bracket)) {
+      stack.push(bracket);
+    } else {
+      const last = stack.pop()!;
+      if (bracket !== bracketMap.get(last)) {
+        return illegalCharacterMap.get(bracket)!;
+      }
+    }
+  }
+  return stack
+    .reverse()
+    .reduce((acc, curr) => acc * 5 + closingCharacterMap.get(curr)!, 0);
 }
